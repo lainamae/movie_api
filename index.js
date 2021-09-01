0a75fdb812761858779738d9c40279b9c2550143
lainamae <64325945+lainamae@users.noreply.github.com>
2021-08-31 20:26:16 -0400

    Update index.js
    added database information and mongoose queries

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+// const Movies = Models.Movie;
+
+const Users = Models.User;
+const Movies = Models.Movie;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 const express = require('express'),
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
@@ -5,76 +17,18 @@ const express = require('express'),
 
 const app = express();
 
-let movies = [
-  {
-    title: 'Frozen 2',
-    description: 'Elsa the Snow Queen has an extraordinary gift -- the power to create ice and snow. But no matter how happy she is to be surrounded by the people of Arendelle, Elsa finds herself strangely unsettled. After hearing a mysterious voice call out to her, Elsa travels to the enchanted forests and dark seas beyond her kingdom -- an adventure that soon turns into a journey of self-discovery.',
-    genre: 'Animated',
-    director: 'Jennifer Lee',
-    imageURL: 'images/frozen2.jpeg',
-  },
-  {
-    title: 'The Hobbit: An Unexpected Journey',
-    description:
-      'Bilbo Baggins (Martin Freeman) lives a simple life with his fellow hobbits in the shire, until the wizard Gandalf (Ian McKellen) arrives and convinces him to join a group of dwarves on a quest to reclaim the kingdom of Erebor. The journey takes Bilbo on a path through treacherous lands swarming with orcs, goblins and other dangers, not the least of which is an encounter with Gollum (Andy Serkis) and a simple gold ring that is tied to the fate of Middle Earth in ways Bilbo cannot even fathom.',
-    genre: 'Fantasy',
-    director: 'Peter Jackson',
-    imageURL: 'images/hobbitunexpected.jpg',
-  },
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    description:
-      "Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths.",
-    genre: 'Fantasy',
-    director: 'Chris Columbus',
-    imageURL: 'images/hpsorcersstone.jpeg',
-  },
-  {
-    title: 'Mulan (1998)',
-    description: 'Fearful that her ailing father will be drafted into the Chinese military, Mulan (Ming-Na Wen) takes his spot -- though, as a girl living under a patriarchal regime, she is technically unqualified to serve. She cleverly impersonates a man and goes off to train with fellow recruits. Accompanied by her dragon, Mushu (Eddie Murphy), she uses her smarts to help ward off a Hun invasion, falling in love with a dashing captain along the way.',
-    genre: 'Animated',
-    director: 'Tony Bancroft',
-    imageURL: 'images/mulan.jpeg',
-  },
-  {
-    title: 'Maleficent',
-    description:
-      "As a beautiful young woman of pure heart, Maleficent (Angelina Jolie) has an idyllic life in a forest kingdom. When an invading army threatens the land, Maleficent rises up to become its fiercest protector. However, a terrible betrayal hardens her heart and twists her into a creature bent on revenge. She engages in an epic battle with the invading king's successor, then curses his newborn daughter, Aurora -- realizing only later that the child holds the key to peace in the kingdom.",
-    genre: 'Fantasy',
-    director: 'Robert Stromberg',
-    imageURL: 'images/maleficent.jpg',
-  },
-  {
-    title: 'Lord of the Rings: The Fellowship of the Ring',
-    description:
-      'An ancient Ring thought lost for centuries has been found, and through a strange twist of fate has been given to a small Hobbit named Frodo. When Gandalf discovers the Ring is in fact the One Ring of the Dark Lord Sauron, Frodo must make an epic quest to the Cracks of Doom in order to destroy it. However, he does not go alone. He is joined by Gandalf, Legolas the elf, Gimli the Dwarf, Aragorn, Boromir, and his three Hobbit friends Merry, Pippin, and Samwise. Through mountains, snow, darkness, forests, rivers and plains, facing evil and danger at every corner the Fellowship of the Ring must go. Their quest to destroy the One Ring is the only hope for the end of the Dark Lords reign.',
-    genre: 'Adventure',
-    director: 'Peter Jackson',
-    imageURL: 'images/LOTRfellowship.jpeg',
-  },
-  {
-    title: 'The Nightmare before Christmas',
-    description:
-      "The film follows the misadventures of Jack Skellington, Halloweentown's beloved pumpkin king, who has become bored with the same annual routine of frightening people in the" +
-      ' "real world." When Jack accidentally stumbles on Christmastown, all bright colors and warm spirits, he gets a new lease on life -- he plots to bring Christmas under his control by kidnapping Santa Claus and taking over the role. But Jack soon discovers even the best-laid plans of mice and skeleton men can go seriously awry.',
-    genre: 'Fantasy',
-    director: 'Tim Burton',
-    imageURL: 'images/nightmarebeforechristmas.jpeg',
-  },
-];
-
 app.use(morgan('common'));
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(methodOverride());
 
 // GET requests
 app.get('/', (req, res) => {
-  res.send('Welcome to my movie club!');
+  res.send('Welcome to myFlix!');
 });
 
 app.use(express.static('public'));
@@ -84,67 +38,169 @@ app.get('static/documentation.html', (req, res) => {});
 // get a list of all movies
 
 app.get('/movies', (req, res) => {
-  res.json(movies);
-  res.send('Successful GET request returning data on all the movies');
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // get data about a single movie by title
 
-app.get('/movies/:title', (req, res) => {
-  res.send('Successful GET request returning data on movie by title.');
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // return data about a genre by name/title
 
-app.get('/movies/genres/:genre', (req, res) => {
-  res.send('Successful GET request returning data on a single genre');
+app.get('/genres/:Name', (req, res) => {
+  Genres.findOne({ Name: req.params.Name })
+    .then((genre) => {
+      res.json(genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // return data about a director by name
 
-app.get('/movies/directors/:director', (req, res) => {
-  res.send('Successful GET request returning data on a single director');
+app.get('/directors/:Name', (req, res) => {
+  Directors.findOne({ Name: req.params.Name })
+    .then((director) => {
+      res.json(director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// add a new user
+// USERS
 
+// USERS - POST NEW USER
 app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.username) {
-    const message = 'Missing "username" in request body.';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
-  }
-
-  res.send('Successful POST request adding new user.');
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-// update user info (username)
-
-app.put('/users/:ID/info', (req, res) => {
-  res.send('Successful PUT request returning data on user info');
+// USERS - GET by USERNAME
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// add movie to a user's favorites
-
-app.put('/users/:ID/favorites', (req, res) => {
-  res.send('Successful PUT request returning data on user favorites');
+// USERS - update user info
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error:' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
-// remove a movie by ID
-
-app.delete('/users/:ID/favorites', (req, res) => {
-  res.send('Successful DELETE request removing data from user favorites');
+// add movie to user favorites by ID
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    { $push: { FavoriteMovies: req.params.MovieID } },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
-// allow users to deregister
+// // remove a movie from user favorites by ID
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    { $pull: { FavoriteMovies: req.params.MovieID } },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
 
-app.delete('/users/:ID/deactivate', (req, res) => {
-  res.send('Successful DELETE request removing user');
+// delete user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // status & error
