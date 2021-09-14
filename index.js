d04f174d7b6ca23191572d42bb6dde669312d5ad
lainamae <64325945+lainamae@users.noreply.github.com>
2021-09-14 16:46:18 -0400

    Update index.js
    temporarily disable jwt auth for /movies endpoint

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('static/documentation.html', (req, res) => {});
 
 // get a list of all movies
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
